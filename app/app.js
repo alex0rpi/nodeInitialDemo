30583a8855122d196d0ad3cdaf9e0e32b82b0d2a
alex0rpi <orpi.alex@hotmail.com>
2023-02-28 09:44:05 +0100

    first 4.1 commit alex

--- app/app.js
+++ app/app.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+const express = require('express');
+
+const app = express();
+
+app.get('/user', (req, res) => {
+  res.json({
+    nom: 'Alex',
+    edat: 49,
+    url: req.url,
+  });
+});
+app.post('/upload', (req, res) => {
+  res.json({
+    nom: 'Alex',
+    edat: 49,
+    url: req.url,
+  });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server is listening to port ${PORT}`));
