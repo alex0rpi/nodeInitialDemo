8e968ce2bc8c687e01c4510c7283c2cae55d067d
alex0rpi <orpi.alex@hotmail.com>
2023-03-01 18:35:36 +0100

    4.2 1st commit

--- app/app.js
+++ app/app.js
@@ -1,27 +1,10 @@
 const express = require('express');
-const fileUpload = require('express-fileupload');
 const routes = require('./routes/myRoutes');
-const path = require('path');
 
 const app = express();
 
 // Enable json parsing
 app.use(express.json());
-
-/*Treat the CORS ERROR*/
-app.use((req, res, next) => {
-  //add certain headers to the response, before we forward the request to the different routes bellow.
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Content-Type', 'application/octet-stream');
-  next();
-});
-/*END of CORS ERROR treating*/
 
 app.use('/', routes);
 
