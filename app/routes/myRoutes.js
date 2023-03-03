f18a7cb617d933b5d74b79f102f17052282bd943
alex0rpi <orpi.alex@hotmail.com>
2023-03-03 10:00:12 +0100

    fileupload

--- app/routes/myRoutes.js
+++ app/routes/myRoutes.js
@@ -14,8 +14,6 @@ router.post('/upload', uploadFile);
 
 router.post('/time', addCacheControl, checkHeader, timeController);
 
-/* Crea una petició GET a l'endpoint /pokemon/{id} que rebi un número de Pokémon, 
-faci una cerca al Pokémon API i retorni el nom del Pokémon, la seva alçada i el seu pes. */
 router.get('/pokemon/:id', findPokemon)
 
 module.exports = router;
