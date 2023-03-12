65dc3256391b1bfa727e602f2744432e127cd1f9
David Iglesias <im.david85@gmail.com>
2023-03-12 16:58:35 +0100

    modificar tarea sequelize completado

--- app/controllers/sequelize.js
+++ app/controllers/sequelize.js
@@ -1,63 +1,70 @@
-const { Task } = require("../models/sequelize");
-const { showUsers } = require("../helpers/showUsers.js");
-const { showUserTasks } = require("../helpers/showUserTasks.js");
+const { Task } = require("../models/sequelize")
+const { showUsers } = require("../helpers/showUsers.js")
+const { showUserTasks } = require("../helpers/showUserTasks.js")
 const { listDeletableTasks } = require("./../helpers/inquirer.js")
 
 const seq_createTask = async (taskData) => {
-  const { user, title, description } = taskData;
+  const { user, title, description } = taskData
   try {
     await Task.create({
       user,
       title,
       description,
       status: "pending",
-    });
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
     console.log({
       message: "Something went wrong",
       data: {},
-    });
+    })
   }
-};
+}
 
 const seq_listTasks = async () => {
   try {
-    const tasks = await Task.findAll();
-    console.log();
+    const tasks = await Task.findAll()
+    console.log()
     tasks.forEach((task, i) => {
-      const index = `${i + 1}`.green;
+      const index = `${i + 1}`.green
       const started = !task.completedIn
         ? task.startedIn
           ? `- In progress`.yellow
           : "- Not started".red
-        : "";
+        : ""
       console.log(
         `${index} User: ${task.user.cyan} ${task.title} --> ${task.status} ${started}`
-      );
-    });
+      )
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
     console.log({
       message: "Something went wrong",
       data: {},
-    });
+    })
   }
-};
+}
 
 const seq_showUserTasks = async () => {
-  const tasks = await Task.findAll();
-  const userString = await showUsers(tasks);
-  const userTasks = tasks.filter((element => element.user === userString))
+  const tasks = await Task.findAll()
+  const userString = await showUsers(tasks)
+  const userTasks = tasks.filter((element) => element.user === userString)
   return await showUserTasks(userTasks)
-};
+}
 
-const seq_deletableTasks = async() => {
+const seq_deletableTasks = async () => {
   const tasks = await Task.findAll()
   const id = listDeletableTasks(tasks)
   return id
 }
 
-const seq_deleteTask = async(ids = []) => await Task.destroy({where: {id: ids}})
+const seq_deleteTask = async (ids = []) =>
+  await Task.destroy({ where: { id: ids } })
 
-module.exports = { seq_createTask, seq_listTasks, seq_showUserTasks, seq_deletableTasks, seq_deleteTask };
+module.exports = {
+  seq_createTask,
+  seq_listTasks,
+  seq_showUserTasks,
+  seq_deletableTasks,
+  seq_deleteTask,
+}
