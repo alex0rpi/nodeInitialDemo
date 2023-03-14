b6a0917191b4cacd3ed534f184db15d0bddd37b3
alex0rpi <orpi.alex@hotmail.com>
2023-03-14 12:58:44 +0100

    ranking done

--- app/controllers/gameControllers.js
+++ app/controllers/gameControllers.js
@@ -1,5 +1,6 @@
 const { Partides } = require('../models');
 const { Users } = require('../models');
+const db = require('../models');
 
 const userPlays = async (req, res) => {
   const playerId = req.params.id;
@@ -40,39 +41,81 @@ const getUserGames = async (req, res) => {
 const getRanking = async (req, res) => {
   try {
     const users = await Users.findAll({ raw: true });
-    // console.log(users);
-    let rankingArray = [];
+    let dataArray = [];
     // users.forEach(async (user) => { // for each seems it doesn't allow to await promises
     for (let user of users) {
       // for...of loop allows the await keyword to be used on the async functions within it.
       let winRatio = 0;
       numberOfGames = await Partides.count({ where: { UserId: user.id } });
+      console.log(numberOfGames)
       numberOfWins = await Partides.count({ where: { UserId: user.id, guanya: true } });
       if (!(numberOfGames === 0)) winRatio = numberOfWins / numberOfGames;
-      rankingArray.push({
+      dataArray.push({
         player: user.username,
-        winRatio: +winRatio,
+        winRatio,
         description: `${numberOfWins} wins out of ${numberOfGames} games played`,
       });
     }
-    let meanWinRatio = 0;
-    const resultRanking = rankingArray
+    let avgWinRatio = 0;
+    const resultRanking = dataArray
       .sort((a, b) => b.winRatio - a.winRatio)
       .map((player) => {
-        meanWinRatio += player.winRatio;
+        avgWinRatio += player.winRatio;
         return {
           ...player,
           winRatio: `${(player.winRatio * 100).toFixed(2)}%`,
         };
       });
-    meanWinRatio = `${((meanWinRatio / users.length)*100).toFixed(2)}%`;
-    return res.status(202).json({ message: 'raking', meanWinRatio, resultRanking });
+    avgWinRatio = `${((avgWinRatio / users.length) * 100).toFixed(2)}%`;
+    return res.status(202).json({ message: 'raking', avgWinRatio, resultRanking });
   } catch (error) {
     return res.status(500).json({ message: 'error', error });
   }
 };
 
-// Aquesta solució ⬇ ⬇, empra programació "declarativa", i només realitza una sola crida a la base de dades. Això, si, la querie queda bastant complexa.
+// Aquesta solució ⬇ ⬇, empra programació "declarativa", i només realitza una sola crida a la base de dades.
+// Això, si, la querie queda bastant complexa i de fet no me n'he ensortit :/
+
+/* const getRanking2 = async (req, res) => {
+  try {
+    const userRaking = await Users.findAll(
+      {
+        include: {
+          model: Partides,
+          attributes: [
+            [
+              db.sequelize.fn('COUNT', db.sequelize.literal(`CASE WHEN "guanya" = true THEN 1 ELSE NULL END`)),
+              'totalWins',
+            ],
+            [db.sequelize.fn('COUNT', db.sequelize.col('UserId')), 'totalGames'],
+            [
+              db.sequelize.fn(
+                'ROUND', db.sequelize.fn( 'COALESCE', db.sequelize.fn( 'CAST', // coalesce to avoid null values
+                    db.sequelize.fn('COUNT', db.sequelize.literal(`CASE WHEN "guanya" = true THEN 1 ELSE NULL END`)) /
+                      db.sequelize.fn('NULLIF', db.sequelize.fn('COUNT', db.sequelize.col('UserId')), 0) // nullif to avoid division by zero
+                  ) * 100,
+                  0 // 0 if null
+                ),
+                2 //round to two decimals
+              ),
+              'winRatio',
+            ],
+          ],
+          group: ['users.id'],
+          order: [[db.sequelize.literal('winRatio DESC')]],
+        },
+      },
+      {raw: true}
+    );
+    console.log(userRaking);
+    return res.status(202).json({ message: 'raking', userRaking });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'error', error });
+  }
+}; */
+
+// ----------------------------------------------------------------------------------
 
 const getWorstPlayer = (req, res) => {};
 
