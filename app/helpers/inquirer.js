d14dccab6d6f97930eba581fa1ce694453c9634b
alex0rpi <orpi.alex@hotmail.com>
2023-03-16 09:18:15 +0100

    igualado comportamiento en json respecto started tasks alex

--- app/helpers/inquirer.js
+++ app/helpers/inquirer.js
@@ -1,23 +1,23 @@
-const inquirer = require("inquirer");
-const colors = require("colors");
+const inquirer = require('inquirer');
+const colors = require('colors');
 
 const questions = [
   {
-    type: "list",
-    name: "option",
-    message: "What do you want to do?",
+    type: 'list',
+    name: 'option',
+    message: 'What do you want to do?',
     choices: [
-      { value: "1", name: `${"1.".yellow} Create task` },
-      { value: "2", name: `${"2.".yellow} List all tasks` },
-      { value: "3", name: `${"3.".yellow} List completed tasks` },
-      { value: "4", name: `${"4.".yellow} List pending tasks` },
-      { value: "5", name: `${"5.".yellow} Mark task(s) started` },
-      { value: "6", name: `${"6.".yellow} Mark task(s) completed` },
-      { value: "7", name: `${"7.".yellow} Delete task` },
-      { value: "8", name: `${"8.".yellow} Show specific task` },
-      { value: "9", name: `${"9.".yellow} Select tasks by user` },
-      { value: "10", name: `${"10.".yellow} Modify task` },
-      { value: "0", name: `${"0.".yellow} Exit` },
+      { value: '1', name: `${'1.'.yellow} Create task` },
+      { value: '2', name: `${'2.'.yellow} List all tasks` },
+      { value: '3', name: `${'3.'.yellow} List completed tasks` },
+      { value: '4', name: `${'4.'.yellow} List pending tasks` },
+      { value: '5', name: `${'5.'.yellow} Mark task(s) started` },
+      { value: '6', name: `${'6.'.yellow} Mark task(s) completed` },
+      { value: '7', name: `${'7.'.yellow} Delete task` },
+      { value: '8', name: `${'8.'.yellow} Show specific task` },
+      { value: '9', name: `${'9.'.yellow} Select tasks by user` },
+      { value: '10', name: `${'10.'.yellow} Modify task` },
+      { value: '0', name: `${'0.'.yellow} Exit` },
     ],
     pageSize: 11,
   },
@@ -26,9 +26,9 @@ const questions = [
 // Menu principal -------------------------------------------------------------------
 const inquirerMenu = async () => {
   console.clear();
-  console.log("=======================".yellow);
-  console.log("   Select an option   ".green);
-  console.log("=======================\n".yellow);
+  console.log('======================='.yellow);
+  console.log('   Select an option   '.green);
+  console.log('=======================\n'.yellow);
 
   // tiene que coincidir el nombre de la const con el "name" en la question (option)
   const { option } = await inquirer.prompt(questions);
@@ -39,9 +39,9 @@ const inquirerMenu = async () => {
 const pause = async () => {
   const question = [
     {
-      type: "input",
-      name: "enter",
-      message: `Please ${"ENTER".red} to continue`,
+      type: 'input',
+      name: 'enter',
+      message: `Please ${'ENTER'.red} to continue`,
     },
   ];
   // console.log('\n');
@@ -51,11 +51,11 @@ const pause = async () => {
 const registerUser = async (user) => {
   const question = [
     {
-      type: "input",
-      name: "userName",
+      type: 'input',
+      name: 'userName',
       user,
       validate(value) {
-        if (value.length === 0) return "Please write something";
+        if (value.length === 0) return 'Please write something';
         return true;
       },
     },
@@ -67,13 +67,12 @@ const registerUser = async (user) => {
 const readInput = async (message) => {
   const question = [
     {
-      type: "input",
-      name: "newText",
+      type: 'input',
+      name: 'newText',
       message,
       validate(value) {
-        if (message === "Title: ") {
-          if (value.length === 0 || value.trim() === "")
-            return "Please write something";
+        if (message === 'Title: ') {
+          if (value.length === 0 || value.trim() === '') return 'Please write something';
           return true;
         }
         return true;
@@ -98,14 +97,14 @@ const listDeletableTasks = async (tasks = []) => {
 
   choices.unshift({
     value: 0,
-    name: `${"0.".green} Cancel`,
+    name: `${'0.'.green} Cancel`,
   });
 
   const preguntas = [
     {
-      type: "checkbox",
-      name: "ids",
-      message: "Delete",
+      type: 'checkbox',
+      name: 'ids',
+      message: 'Delete',
       choices,
     },
   ];
@@ -117,8 +116,8 @@ const listDeletableTasks = async (tasks = []) => {
 const confirm = async (message) => {
   const question = [
     {
-      type: "confirm",
-      name: "ok",
+      type: 'confirm',
+      name: 'ok',
       message,
     },
   ];
@@ -144,9 +143,9 @@ const checklistCompletableTasks = async (tasks = []) => {
 
   const preguntas = [
     {
-      type: "checkbox",
-      name: "ids",
-      message: "Select",
+      type: 'checkbox',
+      name: 'ids',
+      message: 'Select',
       choices,
     },
   ];
@@ -156,15 +155,17 @@ const checklistCompletableTasks = async (tasks = []) => {
 };
 
 const listStartableTasks = async (tasks = []) => {
-  const choices = tasks.map((taskItem, i) => {
-    const idx = `${i + 1}.`.green;
-    const user = `user: ${taskItem.user}`.cyan;
-    return {
-      value: taskItem.id,
-      name: `${idx} ${user} - ${taskItem.title}`,
-      checked: taskItem.startedIn ? true : false,
-    };
-  });
+  const choices = tasks
+    .filter((task) => !task.completedIn)
+    .map((taskItem, i) => {
+      const idx = `${i + 1}.`.green;
+      const user = `user: ${taskItem.user}`.cyan;
+      return {
+        value: taskItem.id,
+        name: `${idx} ${user} - ${taskItem.title}`,
+        checked: taskItem.startedIn ? true : false,
+      };
+    });
 
   // choices.unshift({
   //   value: 0,
@@ -173,9 +174,9 @@ const listStartableTasks = async (tasks = []) => {
 
   const preguntas = [
     {
-      type: "checkbox",
-      name: "ids",
-      message: "Select",
+      type: 'checkbox',
+      name: 'ids',
+      message: 'Select',
       choices,
     },
   ];
